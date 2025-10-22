3c63b75245ec470f5c46307f6d272e87c2e80cb4
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-10-22 20:26:02 +0530

    Sitemap generated

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ecohomes.sa', // Replace with your website URL
+    generateRobotsTxt: true, // (optional) generate robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000, // Optional
+};
